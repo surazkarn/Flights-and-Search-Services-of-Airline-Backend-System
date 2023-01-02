12f2975522065d3901ee58d2ee71215def7e1bdc
surazkarn <surazkarn8848@gmail.com>
2023-01-02 21:42:43 +0530

    routes and repo

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const v1ApiRoutes = require('./v1/index');
 
 //whenver u get a route that starts with v1, map it to v1ApiRoutes
-router.use('./v1', v1ApiRoutes);
+router.use('/v1', v1ApiRoutes);
 //when one will call /v1, they will be routed to v1ApiRoutes which is a router object 
 module.exports = router;
