43935e66cea7d04400ca0f789068a6dafd2c0351
surazkarn <surazkarn8848@gmail.com>
2023-01-02 22:13:45 +0530

    route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,7 +5,7 @@ const CityController = require('../../controller/city-controller')
 const router = express.Router();
 
 router.post('/city', CityController.create);
-router.delete('/city:id', CityController.destroy);
+router.delete('/city/:id', CityController.destroy);
 router.get('/city/:id', CityController.get);
 router.patch('/city/:id', CityController.update);
 
