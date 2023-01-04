bb9568daa404802de3157d027d30a04e388fd581
surazkarn <surazkarn8848@gmail.com>
2023-01-04 12:18:41 +0530

    added airport seeders

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 //main purpose of this file is to booot up the server
-const express = require('express');
+const express = require("express");
 //body-parser is an npm module used to process data sent in an HTTP request body. It provides four express middleware for parsing JSON, Text, URL-encoded, and raw data sets over an HTTP request body. Before the target controller receives an incoming request, these middleware routines handle it.
 const bodyparser = require("body-parser");
 
 
 const {PORT} = require('./config/serverConfig');
-const ApiRoutes = require('./routes/index')
+const ApiRoutes = require('./routes/index');
+
+const db = require('./models/index');
+//const {City, Airport} = require('./models/city');
 
 const setupAndStartServer = async () => {
     //create express object
@@ -16,10 +19,13 @@ const setupAndStartServer = async () => {
 
     app.use('/api', ApiRoutes);
 
-    app.listen(PORT,async() => {
+    app.listen(PORT, async() => {
         console.log(`Server started at ${PORT}`); //STRING INTERPOLATION
-    });
 
+        if (process.env.SYNC_DB) {
+            db.sequelize.sync({alter: true});
+        }
+    });
 }
  
 setupAndStartServer();
---
+++ src/seeders/20230103045937-add-airports.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Airports', [
+      {
+        name: 'Kampegowda International Airport',
+        cityID:6,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Mysuru Airport',
+        cityID:6,
+        createdAt: new Date(), 
+        updatedAt: new Date()
+      },
+      {
+        name: 'Mengaluru International Airport',
+        cityID:6,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Indira Gandhi International Airport',
+        cityID:2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
